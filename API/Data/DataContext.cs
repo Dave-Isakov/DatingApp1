b66aa665f30bcf05e4c5a55956b5dcecc8acab9a
Dave-Isakov <larzu57@gmail.com>
2022-06-21 10:51:40 -0400

    LIkes feature

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,5 +10,24 @@ namespace API.Data
         }
 
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<UserLike>().HasKey(key => new {key.SourceUserId, key.LikedUserId});
+
+            builder.Entity<UserLike>()
+                .HasOne(source => source.SourceUser)
+                .WithMany(like => like.LikedUsers)
+                .HasForeignKey(source => source.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<UserLike>()
+                .HasOne(source => source.LikedUser)
+                .WithMany(like => like.LikedByUsers)
+                .HasForeignKey(source => source.LikedUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
